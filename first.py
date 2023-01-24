6201df110facbd20d54d6952f5043a0022a9a31e
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-01-24 16:00:10 +0330

    first

--- first.py
+++ first.py
@@ -1,9 +1,63 @@
-print("hi")
-print("hi")
-print("hi")
-print("hi")
-print("hi")
-print("hi")
-print(3/2)
-print(3*2)
-print(1+2)
+# print("hi")
+# print("hi")
+# print("hi")
+# print("hi")
+# print("hi")
+# print("hi")
+# print(3/2)
+# print(3*2)
+# print(1+2)
+
+
+# primitive type in python : numbers    str    bool
+
+# x = 1
+# print(type(x))
+# x = 'tina'
+# print(type(x))
+# x = "tina"
+# print(type(x))
+# x = ""
+# print(type(x))
+# x = " "
+# print(type(x))
+# x = "a"
+# print(type(x))
+# bool data types
+# x = False
+# x = True
+# number = 1.2
+# print(type(number))
+# number1 = 1
+# number2 = 2
+# result = number1 + number2
+# print("result is:", result)
+# result = number1 - number2
+# print("result is:", result)
+# result = number1 * number2
+# print("result is:", result)
+# result = number1 / number2
+# print("result is:", result)
+
+
+# name = '1'
+
+# conventions to define a variable name in python
+# any combinations from alphebetical charchters and numbers and _ except that we can't use numbers first
+# valid variable names:  name     n1     name1    name_1    _name   _n_1_
+# invalid : 1n   1name   name 1      name one        name-one      name@
+
+# snake case : number_one
+# camel case : numberOne
+# big camel case NumberOne
+
+number_one = 1
+
+string = 'nikan'
+print(string[0])
+print(string[1])
+print(string[2])
+print(string[3])
+print(string[4])
+
+print(string[1:3])
