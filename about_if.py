fe74f34fa6f6710692f111be73b4e265fbbcd242
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-28 15:58:50 +0330

    sadeghi

---
+++ about_if.py
@@ -0,0 +1,12 @@
+a = 5
+b = 4
+
+if a < b:
+    print("s is lower than b")
+
+if a > b:
+    print("a is greater than b")
+
+# %     if
+#  برنامه ای بنویسید که یک عدد را از ورودی دریافت کند در صورتی که عدد زوج باشد پسغام زیر را چاپ کند
+# number is even
---
+++ review1.py
@@ -0,0 +1,56 @@
+# print('hello')
+# print('2+3*4')
+# print(2+3*4)
+# print(type(1))
+# print((2+3)*4)
+
+# print(2+3*2/2-1)
+# print('he said:"some thing"')
+# print("he said:\"some thing\"")
+# print('''he said:"some thing"''')
+
+# print("hello every body.\nwelcome to our python class.")
+# print("id\tname\tscore")
+# print("01\tartin\t100")
+
+# print("hi.\rwelcome to our python class.\rwelcome to our pygame")
+
+# score = 0
+# print("my score is:", score)
+# score = score + 1
+# print("new score is:", score)
+# score += 1
+# print("new score is:", score)
+# score = score - 1
+# print("new score is:", score)
+# score -= 1
+# print("new score is:", score)
+
+# +   -   *   /
+# //
+# print(3/2)
+# print(3//2)
+# # %
+# print(4 % 2)
+# print(5 % 2)
+
+# print(2 ** 3)
+
+# x = float(input('enter a number: '))
+# print(type(x))
+# y = float(input('enter second number: '))
+# print("x+y=", x+y)
+# print("x-y=", x-y)
+# برنامه ای بنویسی که نام دانش اموز را بگیرد و نمرات سه درس رو بگیره و معدل رو محاسبه کند
+# و در انتها پیغامی مشابه زیر پرینت کند
+# arman's average is: 95
+# name = input("enter student's name: ")
+# score1 = float(input('enter first score: '))
+# score2 = float(input('enter second score: '))
+# score3 = float(input('enter third score: '))
+
+# average = (score1 + score2 + score3) / 3
+# print(f"{name}'s average is:{average}")
+
+import math
+print(math.sqrt(4))
