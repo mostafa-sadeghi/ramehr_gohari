7427963d0b5251e9502bc59528811f3c4d1c9da0
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-03-14 15:59:58 +0330

    sadeghi

--- about_if.py
+++ about_if.py
@@ -8,5 +8,11 @@ if a > b:
     print("a is greater than b")
 
 # %     if
-#  برنامه ای بنویسید که یک عدد را از ورودی دریافت کند در صورتی که عدد زوج باشد پسغام زیر را چاپ کند
+#  برنامه ای بنویسید که یک عدد را از ورودی دریافت کند در صورتی که عدد زوج باشد پیغام زیر را چاپ کند
 # number is even
+number = float(input('enter a number:> '))
+if number % 2 == 0:
+    print('numbers is even')
+
+
+
---
+++ about_turtle.py
@@ -0,0 +1,54 @@
+import turtle
+import time
+
+
+s = turtle.Screen()
+s.bgcolor('orange')
+
+p = turtle.Pen()
+p.pencolor('cyan')
+p.pensize(4)
+# begin triangle
+p.fillcolor("#fcb3ff")
+p.begin_fill()
+p.forward(100)
+p.left(120)
+
+p.forward(100)
+p.left(120)
+
+p.forward(100)
+p.left(120)
+p.end_fill()
+# end triangle
+
+# begin square
+p.pencolor('red')
+p.fd(100)
+p.lt(90)
+p.fd(100)
+p.lt(90)
+p.fd(100)
+p.lt(90)
+p.fd(100)
+p.lt(90)
+# end square
+# begin pentagon
+p.pencolor('gray')
+p.fd(100)
+p.lt(72)
+p.fd(100)
+p.lt(72)
+p.fd(100)
+p.lt(72)
+p.fd(100)
+p.lt(72)
+
+p.fd(100)
+p.lt(72)
+# end pentagon
+
+p.circle(40)
+
+
+s.exitonclick()
