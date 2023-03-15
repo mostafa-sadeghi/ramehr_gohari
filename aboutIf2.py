0c8f407f38d0ecdc1d63eb038dcfa58b0e85e2bb
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-03-15 15:58:17 +0330

    sadeghi

---
+++ aboutIf2.py
@@ -0,0 +1,78 @@
+# a = 5
+# b = 4
+
+# print(a == b)
+# a = 5
+# b = 5
+
+# print(a == b)
+
+
+# s1 = 'abc'
+# s2 = 'cde'
+
+# if s1 != s2:
+#     print("s1 and s2 are not equal!")
+
+# a = 0
+# b = 1
+# if not a == b:
+#     print('a and b are not equal!!')
+
+
+# USERNAME = 'admin'
+# PASSW = 'root'
+
+# user_name = input('enter your user name:> ')
+# password = input('enter your password:> ')
+
+
+# if user_name == USERNAME and password == PASSW:
+#     print("account is valid!!!")
+#     print("user name and password is correct.")
+#     print("login was successful")
+
+# else:
+#     print("account is not valid")
+
+
+# a = 5
+# b = 6
+# c = 3
+
+
+# if a>b or a>c:
+#     print("a is greater than b or c")
+
+
+# if a >= b:  # if a > b or a == b
+#     print('a is greater than or eqial to b')
+# if a <= b:  # if a < b or a == b
+#     print('a is lower than or equal to b')
+
+
+# USERNAME = ['admin','root']
+# PASSW = 'root'
+
+# user_name = input('enter your user name:> ')
+# password = input('enter your password:> ')
+
+
+# if (user_name == USERNAME[0] or user_name == USERNAME[1]) and password == PASSW:
+#     print("account is valid!!!")
+#     print("user name and password is correct.")
+#     print("login was successful")
+
+# else:
+#     print("account is not valid")
+
+
+# کشیدن شکل ها
+# compares ages :    < 8 : kid        8<= <12  : junior      12     18    teenager    > 18 adult
+# برنامه بنویسید که نام یک دانشآموز و نمرات سه درس او را از ورودی بگیرد و معدل دانش آموز
+# را محاسبه و پرینت کند
+# اگر معدل
+# > 90   => A
+# > 80   => B
+# > 70   => C
+# > 60   => D
--- about_turtle.py
+++ about_turtle.py
@@ -1,25 +1,25 @@
-import turtle
-import time
+# import turtle
+# import time
 
 
-s = turtle.Screen()
-s.bgcolor('orange')
+# s = turtle.Screen()
+# s.bgcolor('orange')
 
-p = turtle.Pen()
-p.pencolor('cyan')
-p.pensize(4)
+# p = turtle.Pen()
+# p.pencolor('cyan')
+# p.pensize(4)
 # begin triangle
-p.fillcolor("#fcb3ff")
-p.begin_fill()
-p.forward(100)
-p.left(120)
+# p.fillcolor("#fcb3ff")
+# p.begin_fill()
+# p.forward(100)
+# p.left(120)
 
-p.forward(100)
-p.left(120)
+# p.forward(100)
+# p.left(120)
 
-p.forward(100)
-p.left(120)
-p.end_fill()
+# p.forward(100)
+# p.left(120)
+# p.end_fill()
 # end triangle
 
 # begin square
