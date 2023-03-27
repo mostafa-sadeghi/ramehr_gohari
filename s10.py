bb6669312a6ef1f1914c3d052e1d99ba36e52f4f
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-03-27 15:57:42 +0330

    sadeghi

---
+++ s10.py
@@ -0,0 +1,48 @@
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# for n in numbers:
+#     print(n, end='')
+# print()
+# for n in [1, 2, 3, 4, 5, 6]:
+#     print(n, end='-')
+
+# write a program that takes name as input and print all chars of the name with for
+# name = input('enter a name:> ')
+# for n in name:
+#     print(n)
+
+
+# for i in range(4):
+#     print(i)
+
+# for j in range(5):
+#     print(j)
+
+
+# for i in range(4):
+#     print("i",i)
+#     for j in range(5):
+#         print("j",j)
+#     print()
+
+# name = 'roze'
+# print(len(name))
+
+# numbers = [1,2,3,4,5]
+# print(len(numbers))
+
+matrix = [[1, 2, 3], [4, 5, 6]]
+# print(matrix[0][0],end=' ')
+# print(matrix[0][1],end=' ')
+# print(matrix[0][2])
+# print(matrix[1][0],end=' ')
+# print(matrix[1][1],end=' ')
+# print(matrix[1][2])
+
+print("rows: ", len(matrix))
+print("columns: ", len(matrix[0]))
+
+for i in range(len(matrix)):
+    # print(matrix[i])
+    for j in range(len(matrix[0])):
+        print(matrix[i][j], end=' ')
+    print()
