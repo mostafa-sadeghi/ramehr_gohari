4fea1d4acb7d65a56d56ddaee3e0d34e9b8410b0
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-12 14:59:34 +0330

    sadeghi

--- s13.py
+++ s13.py
@@ -26,9 +26,30 @@ while not done:
     # Game logic
     # drawing
     screen.fill(WHITE)
-    pygame.draw.rect(screen, GREEEN, [155, 150, 80, 75], width=10)
-    pygame.draw.li
-    pygame.display.flip()
-    clock.tick(60)
+    # pygame.draw.rect(screen, GREEEN, [155, 150, 80, 75], width=10)
+    # pygame.draw.line(screen, RED, [0,0],[100,100],5)
+    # pygame.display.flip()
+    # clock.tick(60)
 
-pygame.quit()
+    # y_offset = 0
+    # while y_offset < 100:
+    #     pygame.draw.line(screen,RED, [0, 10 + y_offset], [100, 110+y_offset])
+    #     y_offset += 10
+    # for y_offset in range(0,100,10):
+    #     pygame.draw.line(screen,RED, [0, 10 + y_offset], [100, 110+y_offset])
+
+    # for x_offset in range(30,300,30):
+    #     pygame.draw.line(screen, BLACK, [x_offset,100], [x_offset-10, 90])
+    #     pygame.draw.line(screen, BLACK, [x_offset,90], [x_offset-10, 100])
+
+    # pygame.draw.ellipse(screen, BLACK, [20,20,250,100],2)
+
+    # pygame.draw.polygon(screen, BLACK, [[100,100], [0,200],[200,200]],5)
+#     score = 100
+#     font = pygame.font.SysFont('Arial', 25, True, False)
+#     text = font.render("Score: "+str(score), True, BLACK)
+#     screen.blit(text, [0, 0])
+
+#     pygame.display.flip()
+
+# pygame.quit()
---
+++ s14.py
@@ -0,0 +1,65 @@
+"""
+* * * * * * * * *
+"""
+
+# print("*", end=' ')
+# print("*")
+# for i in range(10):
+#     print("*", end=' ')
+# ex1 : do it with while
+
+"""
+* * * * * * * * * * 
+* * * * *
+* * * * * * * * * * * * * * * * * * * *
+"""
+# for i in range(10):
+#     print("*", end=' ')
+# print()
+# for i in range(5):
+#     print("*", end=' ')
+# print()
+# for i in range(20):
+#     print("*", end=' ')
+
+
+# ex2 : do it with while loop
+
+"""
+* * * * * * * * * *
+* * * * * * * * * *
+* * * * * * * * * *
+* * * * * * * * * *
+* * * * * * * * * *
+* * * * * * * * * *
+* * * * * * * * * *
+* * * * * * * * * *
+* * * * * * * * * *
+* * * * * * * * * *
+"""
+# for row in range(10):
+#     for column in range(10):
+#         print("*", end=' ')
+#     print()
+
+
+# row = 0
+# while row < 10:
+#     column = 0
+#     while column < 10:
+#         print("*", end=" ")
+#         column = column + 1
+#     print()
+#     row = row + 1
+
+
+# ex3:
+"""
+    *
+   * *
+  * * *
+ * * * *
+* * * * *
+
+"""
+# for  +   while
