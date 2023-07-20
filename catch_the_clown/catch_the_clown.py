24cc6daa2d24df2e169653d24f9612bcf20fbdb7
msa <msai.sadehi@gmail.com>
2023-07-20 18:57:49 +0330

    sadeghi

--- catch_the_clown/catch_the_clown.py
+++ catch_the_clown/catch_the_clown.py
@@ -1,18 +1,19 @@
 import pygame
 import random
 
+pygame.init()
+
 WINDOW_WIDTH = 945
 WINDOW_HEIGHT = 600
 
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Catch the clown game!")
 
-
 FPS = 60
 clock = pygame.time.Clock()
 PLAYER_STARTING_LIVES = 5
 CLOWN_STARTING_VELOCITY = 5
-CLOWN_ACCELARATION = 1
+CLOWN_ACCELERATION = 1
 
 score = 0
 player_lives = PLAYER_STARTING_LIVES
@@ -25,11 +26,46 @@ clown_dy = random.choice([-1, 1])
 BLUE = (1, 175, 209)
 YELLOW = (248, 231, 28)
 
+font = pygame.font.Font("Franxurter.ttf", 32)
+
+title_text = font.render("Catch the Clown", True, BLUE)
+title_rect = title_text.get_rect()
+title_rect.topleft = (50, 10)
+
+score_text = font.render(f"Score: {score}", True, YELLOW)
+score_rect = score_text.get_rect()
+score_rect.topleft = (WINDOW_WIDTH - 150, 10)
+
+lives_text = font.render(f"Lives: {player_lives}", True, YELLOW)
+lives_rect = lives_text.get_rect()
+lives_rect.topleft = (WINDOW_WIDTH - 150, 50)
+
+click_sound = pygame.mixer.Sound("click_sound.wav")
+miss_sound = pygame.mixer.Sound("miss_sound.wav")
+pygame.mixer.music.load("Bad Piggies Theme.mp3")
+pygame.mixer.music.play(-1, 0.0)
+
+background_image = pygame.image.load("background.png")
+background_image_rect = background_image.get_rect()
+background_image_rect.center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2)
+
+clown_image = pygame.image.load("clown.png")
+clown_image_rect = clown_image.get_rect()
+clown_image_rect.center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2)
+
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    display_surface.blit(background_image, background_image_rect)
+    display_surface.blit(title_text, title_rect)
+    display_surface.blit(score_text, score_rect)
+    display_surface.blit(lives_text, lives_rect)
+    display_surface.blit(clown_image, clown_image_rect)
+
+    pygame.display.update()
 
 pygame.quit()
--- game1.py
+++ game1.py
@@ -1,6 +1,14 @@
 # https://www.iconarchive.com/
 # https://pinetools.com/flip-image
+from random import randint
 import pygame
+
+
+def change_coin_position():
+    coin_rect.x = WINDOWS_WIDTH + 100
+    coin_rect.y = randint(75, WINDOW_HEIGHT - 32)
+
+
 pygame.init()
 WINDOWS_WIDTH = 600
 WINDOW_HEIGHT = 600
@@ -19,7 +27,45 @@ display_surface.fill(BLACK)
 
 VELOCITY = 5
 FPS = 60
+PLAYER_STARTING_LIVES = 5
+COIN_STARTING_VELOCITY = 4
+COIN_ACCELERATION = 0.5
+
+
 clock = pygame.time.Clock()
+
+score = 0
+player_lives = PLAYER_STARTING_LIVES
+
+coin_velocity = COIN_STARTING_VELOCITY
+
+
+font = pygame.font.Font("Algerian Regular.ttf", 22)
+
+score_text = font.render(f"Score:{score}", True, GREEN, DARKGREEN)
+score_rect = score_text.get_rect()
+score_rect.topleft = (75, 20)
+
+
+title_text = font.render("Feed the dragon", False, GREEN, DARKGREEN)
+title_rect = title_text.get_rect()
+title_rect.centerx = WINDOWS_WIDTH / 2
+title_rect.y = 20
+
+
+lives_text = font.render(f"Lives: {player_lives}", False, GREEN, DARKGREEN)
+lives_rect = lives_text.get_rect()
+lives_rect.topright = (WINDOWS_WIDTH - 75, 20)
+
+
+game_over_text = font.render(f"Game Over.", False, BLUE)
+game_over_rect = game_over_text.get_rect()
+game_over_rect.center = (WINDOWS_WIDTH/2, WINDOW_HEIGHT/2)
+
+
+continue_text = font.render("press any key to continue...", True, RED)
+continue_rect = continue_text.get_rect()
+continue_rect.center = (WINDOWS_WIDTH/2, WINDOW_HEIGHT/2 + 30)
 
 
 dragon_left_image = pygame.image.load("dragon_left.png")
@@ -31,15 +77,6 @@ dragon_right_image = pygame.image.load("dragon_right.png")
 dragon_right_rect = dragon_right_image.get_rect()
 dragon_right_rect.topright = (WINDOWS_WIDTH, 0)
 
-font1 = pygame.font.SysFont('Terminal', 62)
-text1 = font1.render("Dragon Game!", False, GREEN, DARKGREEN)
-text1_rect = text1.get_rect()
-text1_rect.center = (WINDOWS_WIDTH/2, WINDOW_HEIGHT/2)
-
-font2 = pygame.font.Font('Algerian Regular.ttf', 64)
-text2 = font2.render("Dragon!", False, GREEN)
-text2_rect = text2.get_rect()
-text2_rect.center = (WINDOWS_WIDTH/2, WINDOW_HEIGHT/2 + 100)
 
 sound1 = pygame.mixer.Sound('sound.wav')
 sound1.play()
@@ -49,6 +86,12 @@ player_image = pygame.image.load("dragon_right.png")
 player_rect = player_image.get_rect()
 player_rect.x = 0
 player_rect.bottom = WINDOW_HEIGHT
+
+coin_image = pygame.transform.scale(pygame.image.load("coin.png"), (28, 32))
+coin_rect = coin_image.get_rect()
+change_coin_position()
+
+miss_sound = pygame.mixer.Sound("sound.wav")
 
 pygame.mixer.music.load("bgsound.mp3")
 pygame.mixer.music.play(-1, 0.0)
@@ -67,23 +110,61 @@ while running:
             player_rect.centery = mouse_y
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and player_rect.left > 0:
-        player_rect.x -= VELOCITY
-    if keys[pygame.K_RIGHT] and player_rect.right < WINDOWS_WIDTH:
-        player_rect.x += VELOCITY
+
     if keys[pygame.K_UP] and player_rect.top > 75:
         player_rect.y -= VELOCITY
     if keys[pygame.K_DOWN] and player_rect.bottom < WINDOW_HEIGHT:
         player_rect.y += VELOCITY
 
+    if coin_rect.x < 0:
+        change_coin_position()
+        player_lives -= 1
+        miss_sound.play()
+    else:
+        coin_rect.x -= coin_velocity
+
+    if player_rect.colliderect(coin_rect):
+        score += 1
+        coin_velocity += COIN_ACCELERATION
+        change_coin_position()
+
+    if player_lives == 0:
+        
+        display_surface.blit(game_over_text, game_over_rect)
+        display_surface.blit(continue_text, continue_rect)
+        
+        pygame.display.update()
+        pygame.mixer.music.stop()
+
+        is_paused = True
+        while is_paused:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    score = 0
+                    player_lives = PLAYER_STARTING_LIVES
+                    player_rect.y = WINDOW_HEIGHT/2
+                    coin_velocity = COIN_STARTING_VELOCITY
+                    pygame.mixer.music.play(-1, 0.0)
+                    is_paused = False
+                if event.type == pygame.QUIT:
+                    is_paused = False
+                    running = False
+
+    lives_text = font.render(f"Lives: {player_lives}", False, GREEN, DARKGREEN)
+    score_text = font.render(f"Score:{score}", True, GREEN, DARKGREEN)
+
     display_surface.fill(BLACK)
     display_surface.blit(dragon_left_image, dragon_left_rect)
     display_surface.blit(dragon_right_image, dragon_right_rect)
     display_surface.blit(player_image, player_rect)
-    # display_surface.blit(text1, text1_rect)
-    # display_surface.blit(text2, text2_rect)
+
+    display_surface.blit(score_text, score_rect)
+    display_surface.blit(title_text, title_rect)
+    display_surface.blit(lives_text, lives_rect)
 
     pygame.draw.line(display_surface, WHITE, (0, 75), (WINDOWS_WIDTH, 75), 6)
+
+    display_surface.blit(coin_image, coin_rect)
 
     pygame.display.update()
     clock.tick(FPS)
