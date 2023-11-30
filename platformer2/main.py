4e05864311aeac5268a2f069fa5210ae497cbc2c
msa <msai.sadeghi@gmail.com>
2023-11-30 18:58:34 +0330

    s

--- platformer2/main.py
+++ platformer2/main.py
@@ -4,15 +4,22 @@ from constants import *
 from player import Player
 from world import draw_grid, World
 from levels import level0_data
-
+from button import Button
 
 pygame.init()
+
+
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGTH))
 clock = pygame.time.Clock()
 
 sky_image = pygame.image.load('platformer2\img\sky.png')
 sun_image = pygame.image.load('platformer2\img\sun.png')
+restart_button_image = pygame.image.load('platformer2/img/restart_btn.png')
+
+restart_button = Button(SCREEN_WIDTH/2,SCREEN_HEIGTH/2, restart_button_image)
+
+
 
 blob_group = pygame.sprite.Group()
 lava_group = pygame.sprite.Group()
@@ -30,9 +37,15 @@ while running:
     draw_grid(screen)
     world.draw(screen)
     game_status =player.update(screen, world.tile_list, blob_group, lava_group,game_status)
-    blob_group.update()
-    blob_group.draw(screen)
-    lava_group.update()
+    if game_status == "game_over":
+        restart_button.draw(screen)
+        if restart_button.clicked():
+            game_status = "playing"
+            player.reset(100, SCREEN_HEIGTH-130)
+    else:
+        blob_group.update()
+        lava_group.update()
     lava_group.draw(screen)
+    blob_group.draw(screen)
     pygame.display.update()
     clock.tick(FPS)
--- platformer2/player.py
+++ platformer2/player.py
@@ -6,33 +6,15 @@ from pygame.sprite import Sprite
 class Player(Sprite):
     def __init__(self, x, y):
         super().__init__()
-        self.images_right = []
-        self.images_left = []
-        self.index = 0
-        self.counter = 0
-        self.jumped = False
-        self.inair = False
-        self.ghost_img = pygame.image.load("platformer2/img/ghost.png")
-
-        for i in range(1, 5):
-            image_right = pygame.image.load(f'platformer2/img/guy{i}.png')
-            image_right = pygame.transform.scale(image_right, (40, 80))
-            image_left = pygame.transform.flip(image_right, True, False)
-            self.images_right.append(image_right)
-            self.images_left.append(image_left)
-
-        self.image = self.images_right[self.index]
-        self.rect = self.image.get_rect()
-        self.width = self.image.get_width()
-        self.height = self.image.get_height()
-
-        self.rect.x = x
-        self.rect.y = y
-
-        self.vel_y = 0
-        self.direction = 1
+        self.reset(x,y)
+        
 
     def update(self, screen, tile_list, blob_group, lava_group, game_status):
+        if self.direction == 1:
+            rect = pygame.Rect(self.rect.x + 20, self.rect.y + 5, self.image.get_width()-30,self.image.get_height()-10)
+        else:
+            rect = pygame.Rect(self.rect.x + 10 , self.rect.y+ 5 , self.image.get_width()-30,self.image.get_height()-10)
+
         dx = 0
         dy = 0
         walk_cooldown = 5
@@ -41,6 +23,7 @@ class Player(Sprite):
             if keys[K_SPACE] and not self.jumped and not self.inair:
                 self.vel_y = -15
                 self.jumped = True
+                self.inair = True
             if  not keys[K_SPACE]:
                 self.jumped = False
 
@@ -77,16 +60,16 @@ class Player(Sprite):
             dy += self.vel_y
 
             for tile in tile_list:
-                if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+                if tile[1].colliderect(rect.x + dx, rect.y, self.image.get_width()-30,self.image.get_height()-10):
                     dx = 0
-                if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+                if tile[1].colliderect(rect.x, rect.y + dy, self.image.get_width()-30,self.image.get_height()-10):
                     if self.vel_y < 0:
-                        dy = tile[1].bottom - self.rect.top
+                        dy = tile[1].bottom - rect.top
                         self.vel_y = 0
                     elif self.vel_y >=0:
-                    
+                        self.inair = False
                         self.vel_y = 0
-                        dy = tile[1].top - self.rect.bottom
+                        dy = tile[1].top - rect.bottom
         
             if pygame.sprite.spritecollide(self, blob_group, False):
                 game_status = 'game_over'
@@ -111,5 +94,34 @@ class Player(Sprite):
 
        
         screen.blit(self.image, self.rect)
+        # pygame.draw.rect(screen, (145,10,178), rect, 4)
         return game_status
+
+
+    def reset(self, x,y):
+        self.images_right = []
+        self.images_left = []
+        self.index = 0
+        self.counter = 0
+        self.jumped = False
+        self.inair = False
+        self.ghost_img = pygame.image.load("platformer2/img/ghost.png")
+
+        for i in range(1, 9):
+            image_right = pygame.image.load(f'platformer2/img/boy/Run ({i}).png')
+            image_right = pygame.transform.scale(image_right, (TILE_SIZE*1.3, TILE_SIZE*1.1))
+            image_left = pygame.transform.flip(image_right, True, False)
+            self.images_right.append(image_right)
+            self.images_left.append(image_left)
+
+        self.image = self.images_right[self.index]
+        self.rect = self.image.get_rect()
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+
+        self.rect.x = x
+        self.rect.y = y
+
+        self.vel_y = 0
+        self.direction = 1
 
